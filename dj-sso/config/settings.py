d04abec33ecd55cb5405549de57c45e3f206356c
Monwer, Tarek <u1138824@internal.imsglobal.com>
2023-07-22 14:20:53 +0600

    update sso service urls and hosts

--- dj-sso/config/settings.py
+++ dj-sso/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-oc*dldqbn@h0$@n%xcw$=07z*ykg%7)i5clmr293*0r&$vh#wy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -142,4 +142,3 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ],
 }
-
--- dj-sso/config/urls.py
+++ dj-sso/config/urls.py
@@ -3,9 +3,9 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('sso/admin/', admin.site.urls),
+    path('sso/o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path('sso/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     path('sso/', include('users.urls'), name='users'),
 ]
